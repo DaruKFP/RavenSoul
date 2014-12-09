4e4b944c49690d69115f11c67e835d646749a13f
Jesus De La Paz Baltazar <itachiwwe3@gmail.com>
2014-12-09 07:14:15 -0600

    Servicio de galeria de juegos
    ....

---
+++ MiniGames/servicio/servicio.php
@@ -0,0 +1,39 @@
+<?php
+    require_once "../lib/nusoap.php";
+      
+     function getDatos($parametro1) {
+          $datos =  array();
+          $conexion = mysqli_connect("localhost", "root", "", "minigames");
+          $result = mysqli_query($conexion, "select nombre, descripcion, dir_imagen from juegos");
+          while($row = mysqli_fetch_array($result)){
+               array_push($datos, $row["nombre"]);
+               array_push($datos, $row["descripcion"]);
+               array_push($datos, $row["dir_imagen"]);
+          }
+          return $datos;
+          mysqli_free_result($result);
+          mysqli_close($conexión);
+     }
+        
+    
+      
+    $server = new soap_server();
+    $server->configureWSDL("Servicio web para juegos", "urn:datos");
+      
+    $server->register("getDatos",
+        array("parametro1" => "xsd:string"),
+        array("return" => "xsd:Array"),
+        "urn:datos",
+        "urn:datos#getDatos",
+        "rpc",
+        "encoded",
+        "Servicio web para datos de juegos!");
+
+if (isset($HTTP_RAW_POST_DATA)) { 
+  $input = $HTTP_RAW_POST_DATA; 
+}else{ 
+  $input = implode("\r\n", file('php://input')); 
+}
+ 
+$server->service($input);
+?>
